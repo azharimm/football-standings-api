949fc74634128ebae9193ad0d95ec234d32ce5ce
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-31 11:13:13 +0700

    ^ base route

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+const express = require("express");
+const app = express();
+
+const leagueController = require("./src/controllers/leagueController");
+const seasonController = require("./src/controllers/seasonController");
+
+app.get("/", (req, res) => {
+    res.json({
+        status: true,
+        data: "Success",
+    });
+});
+
+app.get("/leagues", leagueController.leagues);
+app.get("/leagues/:id", leagueController.standings);
+app.get("/seasons", seasonController.seasons);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
