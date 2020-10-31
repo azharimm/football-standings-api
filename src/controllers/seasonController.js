f0cb4687c5dbe5cd53857a7b45bdf59f48f7c867
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-31 15:20:38 +0700

    ^ season controller

--- src/controllers/seasonController.js
+++ src/controllers/seasonController.js
@@ -1,1 +1,26 @@
-exports.index = (req, res) => {}
+const data = require("../data/index");
+
+exports.index = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const sort = req.query.sort || 'asc';
+        const season = req.query.season || new Date().getFullYear();
+        const response = await data.get(`/${id}/standings?sort=rank:${sort}&season=${season}`);
+        const { name, abbreviation, seasons } = response.data;
+        
+        return res.json({
+            status: true,
+            data: {
+                name,
+                desc: 'Available seasons standings data',
+                abbreviation,
+                seasons,
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            data: `Error: ${error}`,
+        });
+    }
+};
