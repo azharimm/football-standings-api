9c1ea0b68b90b8c752ce000972c2f42bd5ce0e60
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-31 17:01:59 +0700

    ^ cors

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const seasonController = require("./src/controllers/seasonController");
 const standingController = require("./src/controllers/standingController");
 const indexController = require("./src/controllers/indexController");
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://azharimm.tk");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
+
 app.get("/", indexController.index);
 app.get("/leagues", leagueController.index);
 app.get("/leagues/:id", leagueController.show);
