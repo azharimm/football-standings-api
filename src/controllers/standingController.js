fd2235c78022fc1c22a9ffc4e153224cb41600a7
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-31 15:01:53 +0700

    ^ standings with query params

--- src/controllers/standingController.js
+++ src/controllers/standingController.js
@@ -3,21 +3,37 @@ const data = require("../data/index");
 exports.index = async (req, res) => {
     try {
         const id = req.params.id;
-        const response = await data.get(`/${id}/standings`);
+        const sort = req.query.sort || 'asc';
+        const season = req.query.season || new Date().getFullYear();
+        const response = await data.get(`/${id}/standings?sort=rank:${sort}&season=${season}`);
         const { name, abbreviation, children } = response.data;
-        let results = {
-            name,
-            abbreviation,
-            seasonDisplay: children[0].abbreviation,
-            season: children[0].standings.season,
-            standings: children[0].standings.entries,
-        };
+
+        if(children.length === 0) {
+            throw new Error(`Standings data not available for this ${season} season!`);
+        }
+
+        let standings = children[0].standings.entries.map((standing) => {
+            let team = standing.team;
+            delete team.links;
+            return {
+                team: standing.team,
+                note: standing.note,
+                stats: standing.stats,
+            };
+        });
+        
         return res.json({
             status: true,
-            data: results,
+            data: {
+                name,
+                abbreviation,
+                seasonDisplay: children[0].abbreviation,
+                season: children[0].standings.season,
+                standings,
+            },
         });
     } catch (error) {
-        return res.json({
+        return res.status(500).json({
             status: false,
             data: `Error: ${error}`,
         });
