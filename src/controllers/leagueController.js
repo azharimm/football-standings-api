d7366887bd432fd3de65eb03515ed043e9a0a987
Azhari Marzan <azhari.marzan@gmail.com>
2020-10-31 13:52:33 +0700

    ^ league controller

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -1,3 +1,24 @@
-exports.leagues = (req, res) => {}
+const leagues = require("../data/leagues");
 
-exports.standings = (req, res) => {}
+exports.index = async (req, res) => {
+    return res.json({
+        status: true,
+        data: leagues,
+    });
+};
+
+exports.show = async (req, res) => {
+    const id = req.params.id;
+    const league = leagues.find(league => league.id === id);
+
+    if(!league) {
+        return res.status(404).json({
+            status: false,
+            data: `Error: league not found`,
+        });
+    }
+    return res.json({
+        status: true,
+        data: league,
+    });
+};
