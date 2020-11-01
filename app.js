b5265123a1d8f945f2f86af083cbea3ec2040b6b
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-01 12:11:53 +0700

    not found handling

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.get("/leagues", leagueController.index);
 app.get("/leagues/:id", leagueController.show);
 app.get("/leagues/:id/standings", standingController.index);
 app.get("/leagues/:id/seasons", seasonController.index);
+app.get("/*", (req, res) => {
+    return res.status(404).json({
+        status: false,
+        message: 'Not found!'
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
